78df65f551f6c316b6a8f32935aeb00afae79a6e
Murakami <c0a24253fb@edu.teu.ac.jp>
2025-11-18 16:39:07 +0900

    5:背景画像の移動

--- pygame_image.py
+++ pygame_image.py
@@ -19,8 +19,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [300, 200])
+        x = tmr 
+        screen.blit(bg_img, [-x, 0]) #背景画像
+        screen.blit(kk_img, [300, 200]) #こうかとん
         pg.display.update()
         tmr += 1        
         clock.tick(10)
